30b75c10f3d2ab361cd66bec3dda29b5231b306e
Shahid Raza <engineerbro0098@gmail.com>
2026-07-12 10:07:28 +0530

    oboard route fixing5

--- src/proxy.js
+++ src/proxy.js
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/api/student/onboard(.*)',
+  // '/api/student/onboard(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
